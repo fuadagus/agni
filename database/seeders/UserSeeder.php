2fbb0501365cb6f92ef3f81a1820e4ff2f635c2d
fuadagus <fuadagussalim@mail.ugm.ac.id>
2024-06-13 04:35:16 +0700

    upload project

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //create multiple users
+        $user = [
+            [
+                'name' => 'Admin',
+                'phone' => '085641173515',
+                'email' => 'fuadagussalim@mail.ugm.ac.id',
+                'password' => bcrypt('12345'),
+            ],
+         
+        ];
+
+        //insert the user into the database
+        DB::table('users')->insert($user);
+
+    }
+
+}
